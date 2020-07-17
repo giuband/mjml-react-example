7c601210e97de79a72696f2832e4d9069398e71b
Giuseppe Bandiera <giuseppe.bandiera@live.com>
2020-07-17 12:01:55 +0200

    Test with react-intl

--- src/email.js
+++ src/email.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { readFileSync } from 'fs';
+import React from "react";
+import { readFileSync } from "fs";
 
 import {
   Mjml,
@@ -12,56 +12,69 @@ import {
   MjmlButton,
   MjmlImage,
   MjmlStyle,
-  MjmlText
-} from 'mjml-react';
+  MjmlText,
+} from "mjml-react";
+import { FormattedMessage, IntlProvider } from "react-intl";
 
-const css = readFileSync('./assets/styles.css').toString();
+const css = readFileSync("./assets/styles.css").toString();
 
-export const generate = () => {
+export const generate = (data) => {
   return (
-    <Mjml>
-      <MjmlHead>
-        <MjmlTitle>Last Minute Offer</MjmlTitle>
-        <MjmlPreview>Last Minute Offer...</MjmlPreview>
-        <MjmlStyle>{css}</MjmlStyle>
-        <MjmlStyle>{`
+    <IntlProvider locale="en">
+      <Mjml>
+        <MjmlHead>
+          <MjmlTitle>Last Minute Offer</MjmlTitle>
+          <MjmlPreview>Last Minute Offer...</MjmlPreview>
+          <MjmlStyle>{css}</MjmlStyle>
+          <MjmlStyle>{`
           .blue-column {
             background-color: blue;
           }
         `}</MjmlStyle>
-        <MjmlStyle inline>{`
+          <MjmlStyle inline>{`
           .red-column {
             background-color: red;
           }
         `}</MjmlStyle>
-      </MjmlHead>
-      <MjmlBody width={500}>
-        <MjmlSection fullWidth backgroundColor="#efefef">
-          <MjmlColumn>
-            <MjmlImage src="https://static.wixstatic.com/media/5cb24728abef45dabebe7edc1d97ddd2.jpg" />
-          </MjmlColumn>
-        </MjmlSection>
-        <MjmlSection>
-          <MjmlColumn>
-            <MjmlButton padding="20px" backgroundColor="#346DB7" href="https://www.wix.com/">
-              I like it!
-            </MjmlButton>
-          </MjmlColumn>
-        </MjmlSection>
-        <MjmlSection>
-          <MjmlColumn cssClass="blue-column">
-            <MjmlText>I am blue</MjmlText>
-          </MjmlColumn>
-          <MjmlColumn cssClass="red-column">
-            <MjmlText>I am red</MjmlText>
-          </MjmlColumn>
-        </MjmlSection>
-        <MjmlSection>
-          <MjmlColumn>
-            <MjmlText><a href="/2">Open Second Template</a></MjmlText>
-          </MjmlColumn>
-        </MjmlSection>
-      </MjmlBody>
-    </Mjml>
+        </MjmlHead>
+        <MjmlBody width={500}>
+          <MjmlSection fullWidth backgroundColor="#efefef">
+            <MjmlColumn>
+              <MjmlImage src="https://static.wixstatic.com/media/5cb24728abef45dabebe7edc1d97ddd2.jpg" />
+            </MjmlColumn>
+          </MjmlSection>
+          <MjmlSection>
+            <MjmlColumn>
+              <MjmlButton
+                padding="20px"
+                backgroundColor="#346DB7"
+                href="https://www.wix.com/"
+              >
+                <FormattedMessage
+                  id="123"
+                  defaultMessage="Hi {user}!"
+                  values={{ user: data.name }}
+                />
+              </MjmlButton>
+            </MjmlColumn>
+          </MjmlSection>
+          <MjmlSection>
+            <MjmlColumn cssClass="blue-column">
+              <MjmlText>I am blue</MjmlText>
+            </MjmlColumn>
+            <MjmlColumn cssClass="red-column">
+              <MjmlText>I am red</MjmlText>
+            </MjmlColumn>
+          </MjmlSection>
+          <MjmlSection>
+            <MjmlColumn>
+              <MjmlText>
+                <a href="/2">Open Second Template</a>
+              </MjmlText>
+            </MjmlColumn>
+          </MjmlSection>
+        </MjmlBody>
+      </Mjml>
+    </IntlProvider>
   );
 };
--- src/server.js
+++ src/server.js
@@ -1,19 +1,21 @@
-import express from 'express';
-import {render} from 'mjml-react';
+import express from "express";
+import { render } from "mjml-react";
 
-import * as email1 from './email';
-import * as email2 from './email-second';
+import * as email1 from "./email";
+import * as email2 from "./email-second";
 
 const port = 3000;
 const app = express();
 
-app.get('/2', (req, res) => {
-  const {html} = render(email2.generate(), {validationLevel: 'soft'});
+app.get("/2", (req, res) => {
+  const { html } = render(email2.generate(), { validationLevel: "soft" });
   res.send(html);
 });
 
-app.get('*', (req, res) => {
-  const {html}  = render(email1.generate(), {validationLevel: 'soft'});
+app.get("*", (req, res) => {
+  const { html } = render(email1.generate({ name: "Giuseppe" }), {
+    validationLevel: "soft",
+  });
   res.send(html);
 });
 
